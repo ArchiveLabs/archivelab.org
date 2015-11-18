0694254fd3b1f0ff9d699980d665d76674a43dd3
Mek Karpeles <michael.karpeles@gmail.com>
2015-11-18 04:03:43 -0800

    basic scaffolding

---
+++ README.rst
@@ -0,0 +1,4 @@
+archivelab
+==========
+
+no description available
---
+++ archivelab/views/endpoints.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+endpoints.py
+~~~~~~~~~~~~
+
+:copyright: (c) 2015 by Anonymous.
+:license: see LICENSE for more details.
+"""
+
+from flask import render_template
+from flask.views import MethodView
+
+
+urls = ()
